7c37996aa1acacedbb5d35dabc5c50855afbe052
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-09-12 17:02:50 -0600

    Update rustdesk-server-install.sh

--- rustdesk-server/rustdesk-server-install.sh
+++ rustdesk-server/rustdesk-server-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Install Rustdesk Server
 
+SERVER=""
+
 # Check for root privileges
 if ! [ $(id -u) = 0 ]; then
    echo "This script must be run with root privileges"
@@ -13,6 +15,7 @@ pkg install -y rustdesk-server
 # Enable services
 sysrc rustdesk_hbbr_enable=YES
 sysrc rustdesk_hbbs_enable=YES
+sysrc rustdesk_hbbs_ip="${SERVER}"
 
 # Start services
 service rustdesk-hbbr start
