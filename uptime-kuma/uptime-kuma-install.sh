8ece80041a143909d8e5af52352edca9ae375c97
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-09-13 09:05:26 -0600

    Update uptime-kuma-install.sh

--- uptime-kuma/uptime-kuma-install.sh
+++ uptime-kuma/uptime-kuma-install.sh
@@ -8,7 +8,6 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 NODE_VERSION="18"
-DATA_DIR="/mnt/data"
 
 # Install packages
 pkg install -y git-lite npm-node${NODE_VERSION}
@@ -17,16 +16,26 @@ pkg install -y git-lite npm-node${NODE_VERSION}
 mkdir -p /mnt/data
 mkdir -p /usr/local/etc/rc.d/
 mkdir -p /var/run/uptimekuma/
-chown -R uptimekuma:uptimekuma /var/run/uptimekuma
-chown -R uptimekuma:uptimekuma /mnt/data
 
 # Install Uptime-Kuma
+if [ "$(ls -A "/mnt/data")" ]; then
+   echo "Existing Uptime-Kuma data detected..."
+   REINSTALL="true"
+fi
 pw user add uptimekuma -c uptimekuma -u 3001 -d /nonexistent -s /usr/bin/nologin
 npm install npm -g
 cd /usr/local/ && git clone https://github.com/louislam/uptime-kuma.git
 cd /usr/local/uptime-kuma && npm run setup
 sed -i '' "s|console.log(\"Welcome to Uptime Kuma\");|process.chdir('/usr/local/uptime-kuma');\n&|" /usr/local/uptime-kuma/server/server.js
 fetch -o /usr/local/etc/rc.d/ https://raw.githubusercontent.com/tschettervictor/bsd-apps/main/uptimekuma/usr/local/etc/rc.d/uptimekuma
+if [ ${REINSTALL} != true ]; then
+   cp -R /usr/local/uptime-kuma/data/* /mnt/data/
+fi
+rm -R /usr/local/uptime-kuma/data
+
+# Change directory ownership
+chown -R uptimekuma:uptimekuma /var/run/uptimekuma
+chown -R uptimekuma:uptimekuma /mnt/data
 chown -R uptimekuma:uptimekuma /usr/local/uptime-kuma
 
 # Enable and start services
