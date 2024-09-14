6ad80de40f724986d4bd03f06f7e44dd58e96cc5
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-09-14 16:35:18 -0600

    Update vaultwarden-install.sh

--- vaultwarden/vaultwarden-install.sh
+++ vaultwarden/vaultwarden-install.sh
@@ -32,7 +32,7 @@ if [ "${REINSTALL}" == "true" ]; then
  	echo "Consult the docs to manually change it if needed."
 else
 	ADMIN_HASH=$(echo -n ${ADMIN_TOKEN} | argon2 "$(openssl rand -base64 32)" -e -id -k 65540 -t 3 -p 4 | sed 's#\$#\$\$#g')
-	sed -i '' "s|youradmintokenhere|${ADMIN_HASH}|" /usr/local/etc/rc.conf.d/vaultwarden
+	sed -i '' "s|youradmintokenhere|'${ADMIN_HASH}'|" /usr/local/etc/rc.conf.d/vaultwarden
 fi
 
 # Enable and Start Services
