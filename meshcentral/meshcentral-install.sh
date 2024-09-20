5256bdfa09be595f87839c1c801bae5dfe2d6038
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-09-19 18:41:06 -0600

    meshcentral-install.sh - minor edits

--- meshcentral/meshcentral-install.sh
+++ meshcentral/meshcentral-install.sh
@@ -3,33 +3,35 @@
 
 NODE_VERSION="20"
 
-# Check for root privileges
+# Check for Root Privileges
 if ! [ $(id -u) = 0 ]; then
    echo "This script must be run with root privileges"
    exit 1
 fi
 
-# Install packages
+# Install Packages
 pkg install -y npm-node"${NODE_VERSION}" node"${NODE_VERSION}"
 
-# Create directories
+# Create Directories
 mkdir -p /usr/local/meshcentral/meshcentral-data
 mkdir -p /usr/local/meshcentral/meshcentral-files
 mkdir -p /usr/local/meshcentral/meshcentral-backups
 mkdir -p /usr/local/etc/rc.d
 mkdir -p /var/run/meshcentral
+mkdir -p /var/log/meshcentral
 
 # Install MeshCentral
 pw user add meshcentral -c meshcentral -u 6374 -s /usr/sbin/nologin -d /home/meshcentral -m
 cd /usr/local/meshcentral && npm install meshcentral
 chown -R meshcentral:meshcentral /usr/local/meshcentral
+chown -R meshcentral:meshcentral /var/log/meshcentral
 chown -R meshcentral:meshcentral /var/run/meshcentral
 fetch -o /usr/local/etc/rc.d/meshcentral https://raw.githubusercontent.com/tschettervictor/bsd-apps/main/meshcentral/includes/meshcentral
 chmod +x /usr/local/etc/rc.d/meshcentral
 
-# Enable and start services
+# Enable and Start Services
 sysrc meshcentral_enable="YES"
-service meshcentral start && sleep 5
+service meshcentral start
 
 echo "---------------"
 echo "Installation complete."
