5fc094ca77cf964f5825a766305c975877ff65a6
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-09-24 11:44:10 -0600

    Update heimdall-install.sh - code cleanup

--- heimdall/heimdall-install.sh
+++ heimdall/heimdall-install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Install Heimdall Dashboard
 
-APP_VERSION="2.6.1"
+APP_NAME="Heimdall"
+HEIMDALL_VERSION="2.6.1"
 PHP_VERSION="83"
 
 # Check for Root Privileges
@@ -16,29 +17,28 @@ pkg install -y caddy php${PHP_VERSION} php${PHP_VERSION}-mbstring php${PHP_VERSI
 # Create Directories
 mkdir -p /usr/local/www
 
-# Fetch Caddyfile
-fetch -o /usr/local/www/Caddyfile https://raw.githubusercontent.com/tschettervictor/bsd-apps/main/heimdall/includes/Caddyfile-nossl
-
 # Download and install Heimdall
 mkdir -p /usr/local/www/html
-fetch -o /tmp https://github.com/linuxserver/Heimdall/archive/v"${APP_VERSION}".tar.gz
-tar zxf /tmp/v"${APP_VERSION}".tar.gz --strip 1 -C /usr/local/www/html/
+fetch -o /tmp https://github.com/linuxserver/Heimdall/archive/v"${HEIMDALL_VERSION}".tar.gz
+tar zxf /tmp/v"${HEIMDALL_VERSION}".tar.gz --strip 1 -C /usr/local/www/html/
 mkdir -p /usr/local/www/html/storage/app/public/icons
 sh -c 'find /usr/local/www/ -type d -print0 | xargs -0 chmod 2775'
 touch /usr/local/www/html/database/app.sqlite
 chmod 664 /usr/local/www/html/database/app.sqlite
+cp /usr/local/www/html/.env.example /usr/local/www/html/.env
+sh -c 'cd /usr/local/www/html/ && php artisan key:generate'
 chown -R www:www /usr/local/www/html/
 
 # Enable and Start Services
+fetch -o /usr/local/www/Caddyfile https://raw.githubusercontent.com/tschettervictor/bsd-apps/main/heimdall/includes/Caddyfile-nossl
 sysrc php_fpm_enable=YES
 sysrc caddy_enable=YES
 sysrc caddy_config=/usr/local/www/Caddyfile
-cp /usr/local/www/html/.env.example /usr/local/www/html/.env
-sh -c 'cd /usr/local/www/html/ && php artisan key:generate'
 service php-fpm start
 service caddy start
 
+# Done
 echo "---------------"
 echo "Installation complete!"
-echo "Heimdall is running on port 80"
+echo "${APP_NAME} is running on port 80"
 echo "---------------"
