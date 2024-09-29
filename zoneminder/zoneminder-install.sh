93f692a011261ccc990e4c1a83c3f887caacac6f
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-09-29 14:59:00 -0600

    Update zoneminder-install.sh - code cleanup

--- zoneminder/zoneminder-install.sh
+++ zoneminder/zoneminder-install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # Install Zoneminder
 
-# Check for root privileges
+# Check for Root Privileges
 if ! [ $(id -u) = 0 ]; then
    echo "This script must be run with root privileges"
    exit 1
 fi
 
 APP_NAME="zoneminder"
-MYSQL_VERSION="80"
-DATABASE="MySQL"
-DB_ROOT_PASSWORD=$(openssl rand -base64 15)
+DB_TYPE="MySQL"
 DB_NAME="zm"
-ZM_USER="zmuser"
-ZM_PASS=$(openssl rand -base64 15)
+DB_USER="zoneminder"
+DB_ROOT_PASSWORD=$(openssl rand -base64 15)
+DB_PASS=$(openssl rand -base64 15)
+MYSQL_VERSION="80"
 
 # Install Packages
 pkg install -y zoneminder fcgiwrap mysql${MYSQL_VERSION}-server openssl nginx
@@ -42,18 +42,22 @@ sysrc zoneminder_enable="YES"
 # Generat SSL Certificate for Nginx
 openssl req -new -newkey rsa:2048 -days 366 -nodes -x509 -subj "/O=ZoneMinder Home/CN=*" -keyout /usr/local/etc/ssl/key.pem -out /usr/local/etc/ssl/cert.pem
 
-# Start services (zoneminder will be started later)
+# Start Services (zoneminder will be started later)
 service mysql-server start
 service nginx start
 service php-fpm start
 service fcgiwrap start 
 
 # Create and Configure Database
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';CREATE DATABASE ${DB_NAME};CREATE USER '${ZM_USER}'@'localhost' IDENTIFIED BY '${ZM_PASS}';GRANT SELECT,INSERT,UPDATE,DELETE ON ${DB_NAME}.* TO '${ZM_USER}'@'localhost';FLUSH PRIVILEGES;";
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mysql -u root -e "CREATE DATABASE ${DB_NAME};"
+mysql -u root -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';"
+mysql -u root -e "GRANT SELECT,INSERT,UPDATE,DELETE ON ${DB_NAME}.* TO '${ZM_USER}'@'localhost';"
+mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root --password=${DB_ROOT_PASSWORD} ${DB_NAME} < /usr/local/share/zoneminder/db/zm_create.sql
 echo "ZM_DB_NAME=${DB_NAME}" > /usr/local/etc/zoneminder/zm-truenas.conf
-echo "ZM_DB_USER=${ZM_USER}" >> /usr/local/etc/zoneminder/zm-truenas.conf
-echo "ZM_DB_PASS=${ZM_PASS}" >> /usr/local/etc/zoneminder/zm-truenas.conf
+echo "ZM_DB_USER=${DB_USER}" >> /usr/local/etc/zoneminder/zm-truenas.conf
+echo "ZM_DB_PASS=${DB_PASS}" >> /usr/local/etc/zoneminder/zm-truenas.conf
 
 # Fetch Config Files
 fetch -o /usr/local/etc/php.ini https://raw.githubusercontent.com/tschettervictor/bsd-apps/main/zoneminder/includes/php.ini
@@ -63,25 +67,25 @@ fetch -o /usr/local/etc/nginx/nginx.conf https://raw.githubusercontent.com/tsche
 fetch -o /usr/local/etc/nginx/conf.d/zoneminder.conf https://raw.githubusercontent.com/tschettervictor/bsd-apps/main/zoneminder/includes/zoneminder.conf.nginx.ssl
 fetch -o /usr/local/etc/mysql/conf.d/zoneminder.cnf https://raw.githubusercontent.com/tschettervictor/bsd-apps/main/zoneminder/includes/my.cnf
 
-# Restart Services and start Zoneminder
+# Restart Services and Start Zoneminder
 service mysql-server restart
 service fcgiwrap restart 
 service php-fpm restart
 service nginx restart
 service zoneminder start
 
-# Save passwords for later reference
-echo "${DATABASE} root user is root and password is ${DB_ROOT_PASSWORD}" > /root/${APP_NAME}_passwords.txt
-echo "Zoneminder database user is ${ZM_USER} and password is ${ZM_PASS}" >> /root/${APP_NAME}_passwords.txt
+# Save Passwords
+echo "${DB_TYPE} root user is root and password is ${DB_ROOT_PASSWORD}" > /root/${APP_NAME}-Info.txt
+echo "${APP_NAME} database user is ${DB_USER} and password is ${DB_PASS}" >> /root/${APP_NAME}-Info.txt
 
-
+# Done
 echo "---------------"
 echo "Installation complete."
 echo "---------------"
 echo "Database Information"
-echo "MySQL Username: root"
-echo "MySQL Password: ${DB_ROOT_PASSWORD}"
-echo "Zoneminder DB User: ${ZM_USER}"
-echo "Zoneminder DB Password: ${ZM_PASS}"
+echo "${DB_TYPE} Username: root"
+echo "${DB_TYPE} Password: ${DB_ROOT_PASSWORD}"
+echo "${APP_NAME} DB User: ${ZM_USER}"
+echo "${APP_NAME} DB Password: ${ZM_PASS}"
 echo "---------------"
-echo "All passwords are saved in /root/${APP_NAME}_passwords.txt"
+echo "All passwords are saved in /root/${APP_NAME}-Info.txt"
