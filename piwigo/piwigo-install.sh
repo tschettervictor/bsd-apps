24dcdc45dc7a73a63a6195e973e4dc66da1c2b9b
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-10-16 18:36:21 -0600

    Update piwigo-install.sh - change php-fpm to php_fpm

--- piwigo/piwigo-install.sh
+++ piwigo/piwigo-install.sh
@@ -74,7 +74,7 @@ fetch -o /usr/local/www/Caddyfile https://raw.githubusercontent.com/tschettervic
 sysrc php_fpm_enable=YES
 sysrc caddy_enable=YES
 sysrc caddy_config=/usr/local/www/Caddyfile
-service php-fpm start
+service php_fpm start
 service caddy start
 service mysql-server restart
 
