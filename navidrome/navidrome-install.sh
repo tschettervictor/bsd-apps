cb9be0bb725219b4da6490ca00af86349bd01361
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-12-01 17:17:13 -0700

    Update navidrome-install.sh

--- navidrome/navidrome-install.sh
+++ navidrome/navidrome-install.sh
@@ -9,6 +9,13 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
+# Check for Reinstall
+if [ "$(ls -d /usr/local/etc/navidrome 2>/dev/null)" ]; then
+	echo "Existing ${APP_NAME} config detected."
+	echo "Starting reinstall..."
+	REINSTALL="true"
+fi
+
 # Install Packages
 pkg install -y \
 navidrome
@@ -29,3 +36,8 @@ echo "---------------"
 echo "Installation complete."
 echo "${APP_NAME} is running on port 4533."
 echo "---------------"
+if [ "${REINSTALL}" == "true" ]; then
+	echo "You did a reinstall."
+ 	echo "Please user your old credentials to log in."
+	echo "---------------"
+fi
